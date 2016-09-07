3deffd2f402668ee8d4dd32f295878c113a61447
Hussain <hussain@anattadesign.com>
2016-09-07 11:41:26 +0530

    Scripts changes

--- public/js/components/Header.js
+++ public/js/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Title from "./Header/Title";
 
 export default class Header extends React.Component {
  render() {
+    console.log(this.props) 	
     return (
-     <header>I am heder tag</header>
+    <div>	
+     <Title title={this.props.title} />
+    </div>
     );
   }
 }
--- public/js/components/Layout.js
+++ public/js/components/Layout.js
@@ -1,12 +1,23 @@
 import React from "react";
+
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
- render() {
+constructor (){
+	super();
+	this.state = {
+		title:"Yahya"
+	};
+}
+render() {
+	setTimeout(() => {
+		this.setState({title:"Welcome Yahyaaa!"})
+	}, 2000)
     return (
      <div>
-	     <Header />
+	     <Header title={this.state.title} />
+	     <Header title={"Other title"} />
 	     <hr />
 	     <h1>Layout</h1>
 	     <hr />
